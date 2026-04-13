2abc34baac24cafd8c545d72037feabad24472a9
contactsamharmonie1-netizen <contact.samharmonie1@gmail.com>
2026-04-13 23:51:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sh-v2';
+const CACHE='sh-final-v1';
 const FILES=['./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
